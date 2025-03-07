ca52f8f4cef900e63836775f05525c5a4d97d8f0
Michael Carter <mike.carter@kellogg.northwestern.edu>
2025-03-07 22:23:33 +0000

    completed password encryption and login form, authenticated users. Now need to have login/logout conditionally show up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :current_user
 
   def current_user
-    puts "------------------ code before every request ------------------"
+   @current_user = User.find_by({ "username" => session["username"] })
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   resources "places"
   resources "sessions"
   resources "users"
+  get("/login", { :controller => "sessions", :action => "new" })
+  get("/signup", { :controller => "sessions", :action => "new" })
+  get("/posts", { :controller => "places", :action => "index" })
+  get("/logout", { :controller => "places", :action => "index" })
+  
 end
