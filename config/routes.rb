4f92a7d5b694eabc0f53e810a92f8399f4b84078
Michael Carter <mike.carter@kellogg.northwestern.edu>
2025-03-11 01:21:12 +0000

    first two sections complete

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   get("/login", { :controller => "sessions", :action => "new" })
   get("/signup", { :controller => "sessions", :action => "new" })
   get("/posts", { :controller => "places", :action => "index" })
-  get("/logout", { :controller => "places", :action => "index" })
+  get("/logout", { :controller => "sessions", :action => "destroy" })
   
 end
